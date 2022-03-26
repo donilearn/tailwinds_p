a6efb72ad5059da9b7e0f170538c979d5bdab77f
note_book <note_book@mail.yes>
2022-03-26 17:47:29 +0500

    Added sub components !

---
+++ youtube-clone-tutorial/src/components/categories/theCategory.vue
@@ -0,0 +1,50 @@
+<template>
+  <section
+    class="pt-14 md:pl-24 xl:pl-64 w-full fixed bg-white bg-opacity-95 z-10"
+  >
+    <div class="border-t border-b px-4 max-w-screen-2xl m-auto">
+      <div class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap">
+       
+       <categoryItem v-for="category in categories" :key="category" :category="category" :isActive="category === 'All'"/> 
+
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+
+import categoryItem from './categoryItem.vue'
+
+export default {
+  components:{
+    categoryItem
+  },
+  data() {
+    return {
+      categories: [
+        "All",
+        "Tools",
+        "Trucks",
+        "Machines",
+        "Engines",
+        "Snow",
+        "Ships",
+        "Roads",
+        "Tanks",
+        "Building",
+        "Mars",
+        "Woodworking",
+        "Tractors",
+        "Songs",
+        "TV",
+        "Football",
+        "Planes",
+        "Live",
+        "Streets",
+        "Math",
+        "Programming",
+      ],
+    };
+  },
+};
+</script>
---
+++ youtube-clone-tutorial/src/components/videos/theVideo.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="md:ml-24 xl:ml-64 pt-32 px-5 pb-5">
+    <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-screen-2xl m-auto">
+      
+      <videoItem v-for="i in 20" :key="i" :index="i" />
+    
+    </div>
+  </main>
+</template>
+
+<script>
+import videoItem from './videoItem.vue'
+
+export default {
+  components:{
+    videoItem
+  }
+}
+</script>
